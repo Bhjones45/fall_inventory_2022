3224ec269b8048c33b14d68ca67eebc86c67bad9
Brett Jones <bhjones4545@gmail.com>
2022-01-18 16:00:36 -0700

    Edit item form test

--- spec/features/inventory_items/edit_spec.rb
+++ spec/features/inventory_items/edit_spec.rb
@@ -8,8 +8,25 @@ RSpec.describe 'user can edit an inventory item' do
       visit inventory_item_path(item1.id)
 
       click_on "Update"
+      expect(current_path).to eq(edit_inventory_item_path(item1.id))
+    end
+  end
 
-      expect(current_path).to eq(edit_inventory_item_path)
+  describe 'user fills in form' do
+    it 'can edit the item' do
+      item1 = InventoryItem.create!(name: 'cards', description: '1st edition card', unit_price: 4.70)
+
+      visit edit_inventory_item_path(item1.id)
+
+      fill_in "Name", with: "Trading cards"
+      fill_in "description", with: "Newest released set"
+      fill_in "unit_price", with: 4.70
+      
+      click_on "Save"
+
+      expect(current_path).to eq(inventory_item_path(item1.id))
+      expect(page).to have_content("Trading cards")
+      expect(page).to have_content("Newest released set")
     end
   end
 end

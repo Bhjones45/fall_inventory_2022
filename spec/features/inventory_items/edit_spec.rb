d8bf032a621c0e8025491d1b8a3ed486817b7260
Brett Jones <bhjones4545@gmail.com>
2022-01-18 16:36:34 -0700

    Add more tests for buttons on index page

--- spec/features/inventory_items/edit_spec.rb
+++ spec/features/inventory_items/edit_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'user can edit an inventory item' do
+  describe 'update button can be seen on index page' do
+    it 'can see button' do
+      item1 = InventoryItem.create!(name: 'cards', description: '1st edition card', unit_price: 4.70)
+      item2 = InventoryItem.create!(name: 'binders', description: 'Holds all of your cards', unit_price: 5.00)
+
+      visit inventory_items_path
+      expect(page).to have_button("Update #{item1.name}")
+      expect(page).to have_button("Update #{item2.name}")
+    end
+
+    it 'can click update button' do
+      item1 = InventoryItem.create!(name: 'cards', description: '1st edition card', unit_price: 4.70)
+      item2 = InventoryItem.create!(name: 'binders', description: 'Holds all of your cards', unit_price: 5.00)
+
+      visit inventory_items_path
+
+      click_on "Update #{item1.name}"
+
+      expect(current_path).to eq(edit_inventory_item_path(item1.id))
+    end
+  end
+
   describe 'user clicks button to edit item' do
     it 'shows edit page' do
       item1 = InventoryItem.create!(name: 'cards', description: '1st edition card', unit_price: 4.70)
@@ -21,7 +43,7 @@ RSpec.describe 'user can edit an inventory item' do
       fill_in "Name", with: "Trading cards"
       fill_in "description", with: "Newest released set"
       fill_in "unit_price", with: 4.70
-      
+
       click_on "Save"
 
       expect(current_path).to eq(inventory_item_path(item1.id))

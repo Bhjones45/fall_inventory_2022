d813c9e07ed64c66eb7f7752ba6e3a384db808c0
Brett Jones <bhjones4545@gmail.com>
2022-01-18 14:47:34 -0700

    Add create action and private method

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -6,4 +6,16 @@ class InventoryItemsController < ApplicationController
   def new
     @inventory_item = InventoryItem.new
   end
+
+  def create
+    inventory_item = InventoryItem.new(items_params)
+    inventory_item.save
+    redirect_to '/inventory_items'
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :description, :unit_price)
+  end
 end

56c07ddaa5cd52d4c509ae1ab9129905363a0dc8
Brett Jones <bhjones4545@gmail.com>
2022-01-17 18:18:06 -0700

    Create items index viewing test

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'user sees all inventory items' do
+  describe 'user visits index page' do
+    it 'displays all items' do
+      item1 = InventoryItem.create!(name: 'cards', description: '1st edition card', unit_price: 3.50)
+      item2 = InventoryItem.create!(name: 'binders', description: 'Holds all of your cards', unit_price: 5.00)
+
+      visit '/inventory_items'
+
+      expect(page).to have_content(item1.name)
+      expect(page).to have_content(item1.description)
+      expect(page).to have_content(item1.unit_price)
+      expect(page).to have_content(item2.name)
+      expect(page).to have_content(item2.description)
+      expect(page).to have_content(item2.unit_price)
+    end
+  end
+end

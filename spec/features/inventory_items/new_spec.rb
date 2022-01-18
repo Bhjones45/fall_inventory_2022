7959c80679969a49a9235c888af8357711d147aa
Brett Jones <bhjones4545@gmail.com>
2022-01-18 14:12:31 -0700

    Create form test

--- spec/features/inventory_items/new_spec.rb
+++ spec/features/inventory_items/new_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe 'user creates a new item' do
       click_link 'Create a new inventory item'
 
       expect(current_path).to eq(new_inventory_item_path)
+      fill_in 'name', with: "Floating Flamingo"
+      fill_in 'description', with: 'Float around in style'
+      fill_in 'unit_price', with: '170.99'
+      click_on "Submit"
+
+      expect(page).to have_content('Floating Flamingo')
+      expect(page).to have_content('Float around in style')
+      expect(page).to have_content('170.99')
+      expect(current_path).to eq(inventory_items_path)
     end
   end
 end

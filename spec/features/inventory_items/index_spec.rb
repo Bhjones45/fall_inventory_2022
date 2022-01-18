69a321f331a68df2a8fc83381f3018489ce3d2cb
Brett Jones <bhjones4545@gmail.com>
2022-01-18 13:26:30 -0700

    Refactor routes

--- spec/features/inventory_items/index_spec.rb
+++ spec/features/inventory_items/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'user sees all inventory items' do
       item1 = InventoryItem.create!(name: 'cards', description: '1st edition card', unit_price: 4.70)
       item2 = InventoryItem.create!(name: 'binders', description: 'Holds all of your cards', unit_price: 5.00)
 
-      visit '/inventory_items'
+      visit inventory_items_path
       expect(page).to have_content(item1.name)
       expect(page).to have_content(item1.description)
       expect(page).to have_content(item1.unit_price)

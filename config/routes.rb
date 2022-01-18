39242363af91497f77590cfefd4d96dd9610bfe5
Brett Jones <bhjones4545@gmail.com>
2022-01-17 18:18:32 -0700

    Add route and index method for items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/inventory_items', to: 'inventory_items#index'
 end

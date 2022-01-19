a6a517685aa9e47eb767704828d03d162282afec
Brett Jones <bhjones4545@gmail.com>
2022-01-18 17:07:37 -0700

    Add class method for csv generation with correct attributes

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,4 +1,15 @@
 class InventoryItem < ApplicationRecord
   validates :name, presence: true
   validates :unit_price, presence: true
+
+  def self.to_csv
+    attributes = %w[id name description unit_price created_at updated_at]
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |item|
+        csv << item.attributes.values_at(*attributes)
+      end
+    end
+  end
 end

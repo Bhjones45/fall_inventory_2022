04b41a6330962a3baaffdc5152c276e0ce55b7e1
Brett Jones <bhjones4545@gmail.com>
2022-05-17 14:24:04 -0600

    Create 'location inventory' associations test

--- spec/models/location_inventory_spec.rb
+++ spec/models/location_inventory_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe LocationInventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:inventory_item) }
+    it { should belong_to(:location) }
+  end
 end

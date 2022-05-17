f1cc412f4eec0ef0f0dcbaa4999526dd33f72feb
Brett Jones <bhjones4545@gmail.com>
2022-05-17 14:23:25 -0600

    Create location validations and associations tests

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:location_inventories) }
+    it { should have_many(:inventory_items).through(:location_inventories) }
+  end
 end

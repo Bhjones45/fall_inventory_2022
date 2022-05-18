c47833b12193acd6ecda58fac899fc7614047a1f
Brett Jones <bhjones4545@gmail.com>
2022-05-18 15:47:43 -0600

    Add line break; correct syntax of test

--- spec/features/locations/new_spec.rb
+++ spec/features/locations/new_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe 'user creates a new location' do
   describe 'user fills in form' do
     it 'creates a new item' do
+      item1 = InventoryItem.create!(name: 'cards', description: '1st edition card', unit_price: 4.70)
       visit locations_path
 
       click_link 'Create a new Location'
 
       expect(current_path).to eq(new_location_path)
       fill_in 'name', with: 'Boise'
+      fill_in 'description', with: 'City of trees'
+      check 'inventory_item_ids[]'
       click_on 'Save'
 
       expect(page).to have_content('Boise')

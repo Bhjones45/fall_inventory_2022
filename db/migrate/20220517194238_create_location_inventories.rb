67889b345078e578fcf1c890b4e7d88263b7cf42
Brett Jones <bhjones4545@gmail.com>
2022-05-18 15:58:46 -0600

    Law Abiding Citizen

--- db/migrate/20220517194238_create_location_inventories.rb
+++ db/migrate/20220517194238_create_location_inventories.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateLocationInventories < ActiveRecord::Migration[5.2]
   def change
     create_table :location_inventories do |t|
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,35 +12,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_194238) do
-
+ActiveRecord::Schema.define(version: 20_220_517_194_238) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "inventory_items", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.decimal "unit_price", precision: 8, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'inventory_items', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.decimal 'unit_price', precision: 8, scale: 2
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "location_inventories", force: :cascade do |t|
-    t.bigint "inventory_item_id"
-    t.bigint "location_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["inventory_item_id"], name: "index_location_inventories_on_inventory_item_id"
-    t.index ["location_id"], name: "index_location_inventories_on_location_id"
+  create_table 'location_inventories', force: :cascade do |t|
+    t.bigint 'inventory_item_id'
+    t.bigint 'location_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['inventory_item_id'], name: 'index_location_inventories_on_inventory_item_id'
+    t.index ['location_id'], name: 'index_location_inventories_on_location_id'
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'locations', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "location_inventories", "inventory_items"
-  add_foreign_key "location_inventories", "locations"
+  add_foreign_key 'location_inventories', 'inventory_items'
+  add_foreign_key 'location_inventories', 'locations'
 end
